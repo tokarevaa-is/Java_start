ddda724070c5469a263f5cb2b0d425c446e2dbf1
Andrey Tokarev <aa.tokarev@it-solutions.su>
2022-11-08 00:09:41 +0300

    Initial commit

---
+++ Lesson_1/MyFirstApp.java
@@ -0,0 +1,9 @@
+import java.nio.charset.Charset;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Кукусики!");
+        System.out.println(Charset.defaultCharset().displayName());
+    }
+ }
